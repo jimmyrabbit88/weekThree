409c42098a59f9b2ac718905bf07385d9fd4bcdf
jimmyrabbit88 <jason.dowling@dtudents.ittralee.ie>
2018-09-25 17:55:11 +0100

    books nearly finished

--- src/MyMethods.java
+++ src/MyMethods.java
@@ -1,8 +1,10 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class MyMethods {
     public static int integerInput(String msg, String title){
-        return Integer.parseInt(JOptionPane.showInputDialog(null, msg, title, 1 ));
+        return Integer.parseInt(JOptionPane.showInputDialog(null, msg, title, 1));
+
     }
 
     public static String strInput(String msg, String title){
@@ -11,5 +13,11 @@ public class MyMethods {
 
     public static float floatInput(String msg, String title){
         return Float.parseFloat(JOptionPane.showInputDialog(null, msg, title, 1 ));
+    }
+
+    public static JTextArea createArea() {
+        JTextArea box1 = new JTextArea();
+        box1.setFont(new Font("monospaced", Font.PLAIN, 12));
+        return box1;
     }
 }
--- src/YourBooks.java
+++ src/YourBooks.java
@@ -1,4 +1,9 @@
+import javax.swing.*;
+
 public class YourBooks {
+
+    static Book[] myBooks = new Book[10];
+    static int counter = 0;
     public static void main(String[] args) {
         int choice = 0;
         while (true){
@@ -20,7 +25,7 @@ public class YourBooks {
 
     public static int startingOptions(){
         int choice =0;
-        choice = MyMethods.integerInput("Please chose an option \n\n1 :: Add new Book \n\n2 :: Display your Books \n\n3 :: Quit", "My Library");
+        choice = MyMethods.integerInput("Please chose an option \n\n1 :: Add Favourite Book \n\n2 :: Display your Books \n\n3 :: Quit", "My Library");
         return choice;
     }
 
@@ -28,11 +33,36 @@ public class YourBooks {
         Book book = new Book();
         book.setTitle(MyMethods.strInput("Please enter the Title of the book you want to add", "Add Title"));
         book.setPrice(MyMethods.floatInput("Please enter the price of the book you want to add", "Add Price"));
-        book.setISBN(MyMethods.strInput("Please enter the ISBN of the book you want to add", "Add ISBN"));
+        String isbn =MyMethods.strInput("Please enter the ISBN of the book you want to add", "Add ISBN");
+        //validateIsbn(isbn);
+        book.setISBN(isbn);
         book.setNoPages(MyMethods.integerInput("Please enter the Number of Pages in the book you want to add", "Add Number of Pages"));
+        myBooks[counter] = book;
+        counter++;
+
+
     }
 
     public static void displayBooks(){
+        JTextArea box1;
+        box1 = MyMethods.createArea();
+        box1.setText(String.format("%-20s%-7s%-15s%-20s\n", "Title", "Price", "ISBN", "Number of Pages"));
+        for(int i = 0; i<counter; i++){
+            Book book = new Book();
+            book = myBooks[i];
+            box1.append(String.format("%-20s%-7s%-15s%-20s\n", book.getTitle(), book.getprice(), book.getIsbn(), book.getNoPages()));
+        }
+        JOptionPane.showMessageDialog(null, box1);
+
+    }
+
+    public static int validateIsbn(String isbn){
+        int len = isbn.length();
+        if(len == 10 || len == 13){
+            return 1;
+        }
+        else
+            return 0;
 
     }
 }
