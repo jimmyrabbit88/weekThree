ac1db376ea18a0ba8ba69c3a2748a2968d01b44b
jimmyrabbit88 <jason.dowling@students.ittralee.ie>
2018-10-04 12:25:12 +0100

    vending machine finished

--- src/VendingMachine.java
+++ src/VendingMachine.java
@@ -1,22 +1,85 @@
-public class VendingMachine {
-    private int tokens = 0;
-    private int cans = 0;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-    public int insertToken(){
+public class VendingMachine {
+    private static int tokens;
+    private static int cans;
+    private static JButton insertToken, printValues, insertCans;
+
+
+
+    public void insertToken(){
         if (cans > 0) {
             tokens++;
             cans--;
-            return 1;
+            JOptionPane.showMessageDialog(null, "You have purchased a can of soda", "Purchase",JOptionPane.INFORMATION_MESSAGE);
         }
         else
-            return 0;
+            JOptionPane.showMessageDialog(null, "Sorry we are out of soda","Error", JOptionPane.ERROR_MESSAGE);
     }
 
-    public String toString(){
-        return "Cans = " + cans + "\nTokens = " + tokens;
+    public void showValues(){
+         JOptionPane.showMessageDialog(null, "Cans = " + cans + "\nTokens = " + tokens);
     }
 
-    public void fillUp(int cans){
-        cans += cans
+    public void fillUp(){
+        int newCans = MyMethods.integerInput("How many cans did you put into the Machine", "Fill Cans");
+        cans += newCans;
+    }
+
+
+
+
+
+    public VendingMachine(){
+        this.tokens = 0;
+        this.cans = 5;
+
+        JFrame VMwin = new JFrame("Vending Machine");
+        //FlowLayout flowLayout = new FlowLayout();
+        VMwin.setLocation(600, 100);
+        VMwin.setLayout(null);
+        VMwin.setSize(215, 450);
+        VMwin.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        insertToken = new JButton("Insert Token");
+        insertToken.setLocation(25, 50);
+        insertToken.setSize(150, 70);
+        VMwin.add(insertToken);
+
+        printValues = new JButton("Print Values");
+        printValues.setLocation(25, 170);
+        printValues.setSize(150, 70);
+        VMwin.add(printValues);
+
+        insertCans = new JButton("Insert Cans");
+        insertCans.setLocation(25, 290);
+        insertCans.setSize(150,70);
+        VMwin.add(insertCans);
+
+        ButtonEventHandler buttonHandler = new ButtonEventHandler();
+        insertToken.addActionListener(buttonHandler);
+        printValues.addActionListener(buttonHandler);
+        insertCans.addActionListener(buttonHandler);
+
+        VMwin.setVisible(true);
+
+
+    }
+
+    private class ButtonEventHandler implements ActionListener {
+        public void actionPerformed(ActionEvent e){
+            if(e.getSource() == insertToken){
+                VmDriver.vm1.insertToken();
+            }
+        else if(e.getSource() == printValues){
+                VmDriver.vm1.showValues();
+            }
+            else if(e.getSource() == insertCans){
+                VmDriver.vm1.fillUp();
+            }
+        }
     }
 }
---
+++ src/VmDriver.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class VmDriver {
+   public static VendingMachine vm1 = new VendingMachine();
+
+
+
+    public static void main(String[] args) {
+        VendingMachine guiApp = new VendingMachine();
+    }
+
+
+
+
+}
